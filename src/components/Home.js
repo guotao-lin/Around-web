0c217465154ee754e6669398ce3a28be439ad7b8
guotao <linguotao33511@gmail.com>
2017-12-03 19:11:47 -0500

    get posts, load posts spinner.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import $ from 'jquery';
 import { Tabs, Button, Spin } from 'antd';
-import { GEO_OPTIONS, POS_KEY } from '../constants';
+import { GEO_OPTIONS, POS_KEY, AUTH_PREFIX, TOKEN_KEY, API_ROOT } from '../constants';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
     state = {
+      posts: [],
       error: '',
       loadingGeoLocation: false,
+      loadingPosts: false,
     }
 
     componentDidMount() {
@@ -29,6 +32,7 @@ export class Home extends React.Component {
       this.setState({ loadingGeoLocation: false, error: '' });
       const {latitude: lat, longitude: lon} = position.coords;
       localStorage.setItem(POS_KEY, JSON.stringify({ lat: lat, lon: lon }));
+      this.loadNearbyPosts();
     }
 
     onFailedLoadGeoLocation = () => {
@@ -40,8 +44,30 @@ export class Home extends React.Component {
         return <div>{this.state.error}</div>;
       } else if (this.state.loadingGeoLocation) {
         return <Spin tip="Loading geo location ..."/>;
+      } else if (this.state.loadingPosts) {
+        return <Spin tip='Loading posts ...'/>;
       }
       return null;
+    }
+
+    loadNearbyPosts = () => {
+      //const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+      const { lat, lon } = { "lat":37.5629917,"lon":-122.32552539999998 };
+      this.setState({ loadingPosts: true });
+      return $.ajax({
+           url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+           method: 'GET',
+           headers: {
+             Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
+           },
+      }).then((response) => {
+        this.setState({ loadingPosts: false, posts: response, error: '' });
+        console.log(response);
+      }, (error) => {
+        this.setState({ loadingPosts: false, error: error.responseText });
+      }).catch((error) => {
+        this.setState({ error: error });
+      });
     }
 
     render() {
