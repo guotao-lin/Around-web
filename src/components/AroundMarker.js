bfa5f356b09d9801520db03cac79579f893c2589
guotao <linguotao33511@gmail.com>
2017-12-06 00:37:10 -0500

    add single state control.

---
+++ src/components/AroundMarker.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Marker, InfoWindow } from "react-google-maps";
+
+export class AroundMarker extends React.Component {
+    state = {
+      isOpen: false,
+    }
+
+    onToggleOpen = () => {
+      this.setState((prevState) => {
+        return {isOpen: !prevState.isOpen};
+      });
+    }
+
+  render() {
+    return (
+        <Marker
+          position={this.props.position}
+          onClick={this.onToggleOpen}
+        >
+          {this.state.isOpen ? <InfoWindow onCloseClick={this.onToggleOpen}>
+            <div>asdfasdf</div>
+          </InfoWindow> : null}
+        </Marker>
+    );
+  }
+}
