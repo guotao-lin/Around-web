3cdae02ad88d149c0d4297f145dfc8bcc7c3af16
guotao <linguotao33511@gmail.com>
2017-12-07 23:01:42 -0500

    fix on zoom and mouse over/out map

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -4,11 +4,23 @@ import { POS_KEY } from '../constants';
 import { AroundMarker } from './AroundMarker';
 
 class AroundMap extends React.Component {
-  onDragEnd = () => {
+  reloadMarkers = () => {
     const center = this.map.getCenter();
     const position = { lat: center.lat(), lon: center.lng() };
-    this.props.loadNearbyPosts(position);
+    this.props.loadNearbyPosts(position, this.getRange());
   }
+
+   getRange = () => {
+     const google = window.google;
+     const center = this.map.getCenter();
+     const bounds = this.map.getBounds();
+     if (center && bounds) {
+       const ne = bounds.getNorthEast();
+       const right = new google.maps.LatLng(center.lat(), ne.lng());
+       return 0.000621371192 * google.maps.geometry.spherical.computeDistanceBetween(center, right);  // Leading constant is to convert value to mile.
+     }
+   }
+
   getMapRef = (map) => {
     this.map = map;
   }
@@ -17,7 +29,8 @@ class AroundMap extends React.Component {
     const pos = JSON.parse(localStorage.getItem(POS_KEY));
     return (
       <GoogleMap
-          onDragEnd={this.onDragEnd}
+          onDragEnd={this.reloadMarkers}
+          onZoomChanged={this.reloadMarkers}
           ref={this.getMapRef}
           defaultZoom={11}
           defaultCenter={{ lat: pos.lat, lng: pos.lon }}
